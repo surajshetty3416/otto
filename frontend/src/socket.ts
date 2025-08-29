4e204de6f53efd44c0a55c8af4fe0309a3c7b187
18alantom <2.alan.tom@gmail.com>
2025-08-29 17:46:00 +0530

    fix type check error

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -3,9 +3,11 @@ import csc from "../../../../sites/common_site_config.json";
 
 export function initSocket() {
   const host = window.location.hostname;
-  const siteName = import.meta.env.DEV ? csc.default_site : window.site_name;
   const port = window.location.port ? `:${csc.socketio_port}` : "";
   const protocol = port ? "http" : "https";
+
+  // @ts-ignore default_site not always available
+  const siteName = import.meta.env.DEV ? csc.default_site : window.site_name;
   const url = `${protocol}://${host}${port}/${siteName}`;
 
   return io(url, {
