5d50c00c4b87830702e891c2adae37ba9113ffcc
18alantom <2.alan.tom@gmail.com>
2025-09-17 11:29:54 +0530

    build email template css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,8 @@
+import preset from "./tailwind/preset";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	presets: [require("./tailwind/preset")],
+	presets: [preset],
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
